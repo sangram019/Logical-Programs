d1f4054296c549b945b57523735fe8feff90e771
Sangram <sangram15.behera@gmail.com>
2022-04-27 03:03:41 +0530

    Added by Sangram.

---
+++ Logical Program/Fibonacci.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalPrograms
+{
+    public class FibonacciSeries
+    {
+        public void fibonacci()
+        {
+            int n1 = 0, n2 = 1, n3, i, number;
+            Console.Write("Enter the number of elements: ");
+            number = int.Parse(Console.ReadLine());
+            Console.WriteLine(n1 + " " + n2 + " ");
+            for (i = 2; i < number; ++i)
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+            }
+        }
+    }
+}
