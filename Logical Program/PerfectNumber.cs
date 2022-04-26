efe59b567e6ed1a90665efb473b9b0e9f5a30676
Sangram <sangram15.behera@gmail.com>
2022-04-27 03:20:31 +0530

    Added by Sangram.

---
+++ Logical Program/PerfectNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace LogicalPrograms
+{
+    public class PerfectNumber
+    {
+        public void number()
+        {
+            {
+                int n, i, a, b, sum;
+                Console.Write("Enter the starting  number : ");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter the ending number : ");
+                b = Convert.ToInt32(Console.ReadLine());
+                Console.Write("The Perfect numbers within the given Numbers : ");
+                for (n = a; n <= b; n++)
+                {
+                    i = 1;
+                    sum = 0;
+                    while (i < n)
+                    {
+                        if (n % i == 0)
+                            sum = sum + i;
+                        i++;
+                    }
+                    if (sum == n)
+                        Console.Write("{0} ", n);
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
