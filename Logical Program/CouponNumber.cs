2ec502e827da3936185ac18be0bc720df88cfbf5
Sangram <sangram15.behera@gmail.com>
2022-04-27 04:24:10 +0530

    Added by Sangram.

---
+++ Logical Program/CouponNumber.cs
@@ -0,0 +1,20 @@
+﻿namespace LogicalPrograms
+{
+    public class CouponNumber
+    {
+        public void coupon()
+        {
+            Console.WriteLine("Enter number of coupons");
+            int number = int.Parse(Console.ReadLine());
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            Random random = new Random();
+            string result = new string(
+                Enumerable.Repeat(chars, 8)
+                          .Select(s => s[random.Next(s.Length)])
+                          .ToArray());
+            Console.Write("Your coupon number is " + result);
+        }
+
+
+    }
+}
